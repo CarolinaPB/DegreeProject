4f1ea864a97db7562e756cd302ef9dda36bff9d5
CarolinaPB <carolina.pitabarros@gmail.com>
2019-12-11 11:32:23 +0100

    Revert "removed array parameter"
    This reverts commit 3af78d13e3760e6d0bc284ecff718f7e1c8faab3.

--- code/2019-12-11/11_12_2019_run_effecteqtl_on_gene.sh
+++ code/2019-12-11/11_12_2019_run_effecteqtl_on_gene.sh
@@ -3,9 +3,10 @@
 #SBATCH -p core #partition
 #SBATCH -n 4 #number of tasks
 #SBATCH -t 12:00:00 #total run time of the job allocation
+#SBATCH --array=1-4
 #SBATCH --output="%A_%a.out"
 #SBATCH --error="%A_%a.error"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user carolina.pitabarros@gmail.com
 
-Rscript /home/carolpb/DegreeProject/code/2019-12-09/09_12_2019_effecteqtl_on_genes.R
+Rscript /home/carolpb/DegreeProject/code/2019-12-09/09_12_2019_effecteqtl_on_genes.R $SLURM_ARRAY_TASK_ID
