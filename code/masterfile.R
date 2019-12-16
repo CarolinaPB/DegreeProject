abdb33029599e675d9cd42456a82cc297901f026
CarolinaPB <carolina.pitabarros@gmail.com>
2019-12-16 09:57:31 +0100

    added info about running scripts on snowy

--- code/masterfile.R
+++ code/masterfile.R
@@ -1,3 +1,12 @@
+# Project code Johan
+# g2019026
+
+# To use Johan's project code, the batch jobs need to be sent to snowy https://www.uppmax.uu.se/support/user-guides/snowy-user-guide/
+# SNOWY
+squeue -M snowy
+sbatch -M snowy slurm_script_file
+--reservation=job_might_be_killed
+
 #   ____________________________________________________________________________
 #   Starting data                                                           ####
 library(data.table)
@@ -8,8 +17,12 @@ eqtl_results <- fread("data/SI_Data_04_eQTL.csv")
 
 #   ____________________________________________________________________________
 #   General info                                                            ####
+
+# expression levels in units of log2(TPM) for all genes and segregants
+
 # 1012 haploid segregants (rows) X 42,052 markers (columns)
 # -1 indicates BY allele, +1 indicates RM allele
+# BY (i.e. reference) alleles are denoted by ‘-1’. RM alleles are denoted by ‘1’
 # column names indicate chromosome:position_BYvariant/RMvariant
 # postitions are based on the S.Cerevisiae SacCer3 genome build
 
@@ -18,3 +31,6 @@ eqtl_results <- fread("data/SI_Data_04_eQTL.csv")
 # are likely to be located near each other on a chromosome and are therefore likely to be inherited. 
 # A LOD score of 3 or higher is generally understood to mean that two genes are located close to each other on the chromosome. 
 # In terms of significance, a LOD score of 3 means the odds are a thousand to one that the two genes are linked, and therefore inherited together.
+
+  
+  
