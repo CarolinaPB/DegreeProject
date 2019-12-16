ff3d0db57ed4860a275e86950d579769f439c4d0
CarolinaPB <carolina.pitabarros@gmail.com>
2019-12-16 09:55:57 +0100

    get effect of eqtl on the corresponding gene

--- code/2019-12-11/11_12_2019_effect_eqtl-gene.R
+++ code/2019-12-11/11_12_2019_effect_eqtl-gene.R
@@ -17,25 +17,27 @@ lmp <- function (modelobject) {
   return(p)
 }
 
-
-
-gene_eqtl.anova <- data.table(matrix(ncol = 4, nrow = nrow(eqtl_results)))
-colnames(gene_eqtl.anova) <- c("gene", "eqtl", "pval", "r2")
+eqtl_results$anova.pval <- NA
+eqtl_results$anova.r2 <- NA
 
 start_time <- Sys.time()
 for (rownum in 1:nrow(eqtl_results)){
   gene <-  eqtl_results$gene[rownum]
   eqtl <- eqtl_results$pmarker[rownum]
-  gene_eqtl.anova$gene[rownum] <- eqtl_results$gene[rownum]
-  gene_eqtl.anova$eqtl[rownum] <- eqtl_results$pmarker[rownum]
   
   anv <- lm(unlist(phenotype[ , ..gene]) ~ factor(unlist(genotype[ ,..eqtl])))
   
-  gene_eqtl.anova$pval[rownum] <- lmp(anv)
-  gene_eqtl.anova$r2[rownum] <- summary(anv)$adj.r.squared
+  eqtl_results$anova.pval[rownum] <- lmp(anv)
+  eqtl_results$anova.r2[rownum] <- summary(anv)$adj.r.squared
 }
 end_time <- Sys.time()
 end_time - start_time
 
-fwrite(gene_eqtl.anova, paste0(path,"results/2019-12-11/effect_eqtl-gene.gz"), sep="\t")
+eqtl_results[cis=="VERDADEIRO"]$cis <- TRUE
+eqtl_results[cis=="FALSO"]$cis <- FALSE
+fwrite(eqtl_results, paste0(path,"results/2019-12-11/eqtl_results_effect_eqtl-gene.gz"), sep="\t")
+
+test <- fread(paste0(path,"results/2019-12-11/eqtl_results_effect_eqtl-gene.gz"))
+eqtl_results[order(-rank(anova.pval))]
+
 
