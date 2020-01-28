8db7e49286863c7a7adea1dac15e57059ad285da
CarolinaPB <carolina.pitabarros@gmail.com>
2020-01-28 15:26:42 +0100

    fixed respath link

--- code/2020-01-28/28_01_2020_nestedcomm.R
+++ code/2020-01-28/28_01_2020_nestedcomm.R
@@ -2,7 +2,7 @@ library(data.table)
 library(linkcomm)
 
 path <- "/home/carolpb/DegreeProject/" # use with uppmax
-respath <- "/proj/snic2019-8-367/private/carol/results"
+respath <- "/proj/snic2019-8-367/private/carol/results/"
 
 find.effects_TF <- fread(paste0(respath, "2020-01-27/findeffects_TF.gz"))
 load(paste0(respath,"2020-01-27/27_01_2020_linkedcommunities_TF.Rdata"))
