e9b9f04b2547e75dd972c28f07275a298cbc40da
CarolinaPB <carolina.pitabarros@gmail.com>
2020-02-11 14:54:31 +0100

    changed running time

--- code/2020-02-10/10_02_2020_run_enrichment_GO.sh
+++ code/2020-02-10/10_02_2020_run_enrichment_GO.sh
@@ -2,8 +2,7 @@
 #SBATCH -A g2019026
 #SBATCH -p core #partition
 #SBATCH -n 4 #number of tasks
-#SBATCH -t 00:15:00 #total run time of the job allocation
-#SBATCH --qos=short
+#SBATCH -t 24:00:00 #total run time of the job allocation
 #SBATCH --output="%A_%a.out"
 #SBATCH --error="%A_%a.error"
 #SBATCH --mail-type=ALL
