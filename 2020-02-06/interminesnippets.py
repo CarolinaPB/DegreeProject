a85cbf7e9059bf03d43b86f5c0edea8befc14750
CarolinaPB <carolina.pitabarros@gmail.com>
2020-02-17 15:07:17 +0100

    Delete interminesnippets.py

--- 2020-02-06/interminesnippets.py
+++
@@ -1,8 +0,0 @@
-# Intermine snippets
-
-
-# To view the names of all the available templates you can view the templates dictionary and iterate through it.
-service = Service("www.flymine.org/flymine/service")
-templates=service.templates
-for name in templates.keys():
-    print(name)
