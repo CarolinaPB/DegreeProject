3e53923e240569fef9d8a24265f5ddf944dee7b9
CarolinaPB <carolina.pitabarros@gmail.com>
2020-02-19 13:55:23 +0100

    Update 13_02_2020_enrichmentanalysis_hypergeo.R

--- code/2020-02-13/13_02_2020_enrichmentanalysis_hypergeo.R
+++ code/2020-02-13/13_02_2020_enrichmentanalysis_hypergeo.R
@@ -15,50 +15,63 @@ phenotype <- fread("/Users/Carolina/Documents/GitHub/DegreeProject/data/SI_Data_
 
 find.effects_TF <- fread(paste0(respath, "2020-01-27/findeffects_TF.gz"))
 
-## table with GO terms + parent terms for each gene
+## table with GO terms + parent terms for each gene + gene evidence code
 ympy.evidence.parents <- fread(paste0(path, "results/2020-02-13/yeastmine_evidencecode_parents.gz"))
 ympy.evidence.parents <- unique(ympy.evidence.parents)
 
+# table with necessary columns for creating a geneset and calculating enrichment
+# NOT USING PARENTS INFO - USING THE "NORMAL" INFO
 goframeData <- unique(ympy.evidence.parents[,.(Gene.goAnnotation.ontologyTerm.identifier, Gene.goAnnotation.evidence.code.code, Gene.secondaryIdentifier)])
 
-
+# creating the sets of genes to use 
+# the "interesting genes" - either genesA (the causal ones) or genesB (on the receiving end)
+# the "universe" - in this case, all the genes in our dataset
 genesA <- unlist(unique(find.effects_TF[,geneA]))
 genesB <- unlist(unique(find.effects_TF[,geneB]))
 universe <- colnames(phenotype[,2:ncol(phenotype)])
 
+# create geneset
 gs <- getgeneset(goframeData)
+# get enrichment for genesA
 res.geneA <- getenrichment(gs, universe = universe, interestinggenes = genesA)
 res.geneA.dt <- data.table(summary(res.geneA))
 #htmlReport(res.geneA.parents, file=paste0(respath, "2020-02-13/GOenrichmentreport_genesA.html"))
 
+# get enrichment for genesB
 res.geneB <- getenrichment(gs, universe = universe, interestinggenes = genesB)
 res.geneB.dt <- data.table(summary(res.geneB))
 
-
+# get list with different graphs that represent relations between GO terms
 termgrA <- termGraphs(res.geneA, use.terms = T, pvalue = 0.05)
-pdf(file = "results/2020-02-13/termgraph_A.pdf", onefile = T)
-for (i in 1:length(termgrA)){
-  plotGOTermGraph(termgrA[[i]], r = res.geneA, add.counts = T, node.colors=c(sig="green", not="white"), max.nchar=30)
-}
-dev.off()
 
+# save all the graphs to a pdf
+# pdf(file = "results/2020-02-13/termgraph_A.pdf", onefile = T)
+# for (i in 1:length(termgrA)){
+#   plotGOTermGraph(termgrA[[i]], r = res.geneA, add.counts = T, node.colors=c(sig="green", not="white"), max.nchar=30)
+# }
+# dev.off()
+
+
+# get list with different graphs that represent relations between GO terms
 termgrB <- termGraphs(res.geneB, use.terms = T, pvalue = 0.05)
-pdf(file = "results/2020-02-13/termgraph_B.pdf", onefile = T)
-for (i in 1:length(termgrB)){
-  plotGOTermGraph(termgrB[[i]], r = res.geneB, add.counts = T, node.colors=c(sig="green", not="white"), max.nchar=30)
-}
-dev.off()
+# pdf(file = "results/2020-02-13/termgraph_B.pdf", onefile = T)
+# for (i in 1:length(termgrB)){
+#   plotGOTermGraph(termgrB[[i]], r = res.geneB, add.counts = T, node.colors=c(sig="green", not="white"), max.nchar=30)
+# }
+# dev.off()
 
 
-#### Conditional hypergeo test
+#### CONDITIONAL HYPERGEO TEST ######
+
+# for genesA as the genes of interest
 paramsCond <- GSEAGOHyperGParams(name="first try",
-                                geneSetCollection=gs,
-                                geneIds = genesA,
-                                universeGeneIds = universe,
-                                ontology = "BP",
-                                pvalueCutoff = 0.05,
-                                conditional = T,
-                                testDirection = "over")
+                                 geneSetCollection=gs,
+                                 geneIds = genesA,
+                                 universeGeneIds = universe,
+                                 ontology = "BP",
+                                 pvalueCutoff = 0.05,
+                                 conditional = T,
+                                 testDirection = "over")
 hgCond = hyperGTest(paramsCond)
 hgCond.dt <- data.table(summary(hgCond))
 
@@ -73,5 +86,51 @@ setdiff(stdIds, condIds)
 terms <- nodes(termgrA[[2]])
 hgCond.dt.sub <- data.table(summary(hgCond, pvalue=0.5)[,c("GOBPID","Term", "Pvalue")])
 
-res.geneA.dt[GOBPID %in% terms, .(GOBPID, Term, Pvalue)][order(GOBPID)] # standard
-hgCond.dt.sub[GOBPID %in% terms][order(GOBPID)] # after conditional 
+hypergeo_compare <- merge(res.geneA.dt[GOBPID %in% terms, .(GOBPID, Term, Pvalue)], hgCond.dt.sub[GOBPID %in% terms][order(GOBPID)], by=c("GOBPID", "Term"), all=T)
+setnames(hypergeo_compare, old=c("Pvalue.x", "Pvalue.y"), new=c("pval", "cond.pval"))
+# Adds new column - if true, it means that that GO term's p-value changed
+hypergeo_compare[, changed:=pval != cond.pval]
+
+hypergeo_compare
+
+# "cellular response to DNA damage stimulus" is no longer significant -  does it mean that we can only say that only the general term DNA repair is significant?
+
+# check if the GO terms are still significant after the conditional hypergeo test
+# first node group
+terms.1 <- nodes(termgrA[[1]])
+
+res.geneA.dt[GOBPID %in% terms.1, .(GOBPID, Term, Pvalue)][order(GOBPID)] # standard
+hgCond.dt.sub[GOBPID %in% terms.1][order(GOBPID)] # after conditional 
+
+
+hypergeo_compare.1 <- merge(res.geneA.dt[GOBPID %in% terms.1, .(GOBPID, Term, Pvalue)], hgCond.dt.sub[GOBPID %in% terms.1][order(GOBPID)], by=c("GOBPID", "Term"), all=T)
+setnames(hypergeo_compare.1, old=c("Pvalue.x", "Pvalue.y"), new=c("pval", "cond.pval"))
+
+hypergeo_compare.1[, changed:=pval != cond.pval]
+hypergeo_compare.1
+
+# terms that changed but are still significant at pval < 0.05
+hypergeo_compare[pval<cond.pval & cond.pval < 0.05]
+
+
+
+
+termgrA_cond <- termGraphs(hgCond, use.terms = T, pvalue = 0.05)
+# pdf(file = "results/2020-02-13/termgraph_A_condhypergeo.pdf", onefile = T)
+# for (i in 1:length(termgrA_cond)){
+#   plotGOTermGraph(termgrA_cond[[i]], r = hgCond, add.counts = T, node.colors=c(sig="green", not="white"), max.nchar=200)
+# }
+# dev.off()
+
+
+
+#One might also want to consider reduction of the universe to exclude genes that are not expressed, if such a determination can be made, since arguments can be made against maintaining objects in the universe that cannot be selected. The
+
+# distance between GO terms in termgrA - graph 1
+# GO_genesA_dist <- compGdist(termgrA[[1]], nodes(termgrA[[1]]), verbose = T)
+# GO_genesA_dist.dt <- data.table(t, keep.rownames=T)
+# 
+# 
+# 
+# prof <- basicProfile(genelist=genesA, idType = "GoTermsFrame", onto="BP", level=2, empty.cats=T)
+# 
